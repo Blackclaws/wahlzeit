59b4de22c162bf8377178a26ce82d37a6f124386
Felix Winterhalter <felix>
2015-10-26 15:22:44 +0100

    Changed Format of Coordinate

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -2,111 +2,70 @@ package org.wahlzeit.model;
 
 /**
  * The coordinate class used for storing geolocations.
+ * 
  * @author Felix
  *
  */
 public class Coordinate {
-	double latitude;
-	double longitude;
-	boolean isResult;
-	
-	public Coordinate()
-	{
-		latitude = 0;
-		longitude = 0;
+    double latitude;
+    double longitude;
+
+    public Coordinate() {
+	latitude = 0;
+	longitude = 0;
+    }
+
+    public Coordinate(double lat, double lon) {
+	if (-90 < lat && lat < 90) {
+	    throw new IllegalArgumentException("Latitude needs to be between -90 and 90 is:" + lat);
 	}
-	
-	public Coordinate(double lat, double lon)
-	{
-		this(lat,lon,false);
+	if (-180 < lon && lon < 180) {
+	    throw new IllegalArgumentException("Longitude needs to be between -180 and 180 is:" + lon);
 	}
-	
-	public Coordinate(double lat, double lon, boolean is_result) throws IllegalArgumentException
-	{
-		if(!is_result)
-		{
-			if(lat > 90 || lat < -90)
-			{
-				throw new IllegalArgumentException("Latitude needs to be between -90 and 90 is:" + lat);
-			}
-			if(lon > 180 || lon < -180)
-			{
-				throw new IllegalArgumentException("Longitude needs to be between -180 and 180 is:" + lon);
-			}
-		}
-		latitude = lat;
-		longitude = lon;
-		isResult = is_result;
+	latitude = lat;
+	longitude = lon;
+    }
+
+    public boolean equals(Object other) {
+	if (other instanceof Coordinate) {
+	    Coordinate otherCoord = (Coordinate) other;
+	    return (otherCoord.getLatitude() != latitude || otherCoord.getLongitude() != longitude);
 	}
-	
-	public boolean equals(Object other)
-	{
-		if(other instanceof Coordinate)
-		{
-			Coordinate otherCoord = (Coordinate) other;
-			if(otherCoord.getLatitude() != latitude || otherCoord.getLongitude() != longitude)
-			{
-				return false;
-			}
-			else
-			{
-				return true;
-			}
-		}
-		return false;
+	return false;
+    }
+
+    public double getLatitude() {
+	return latitude;
+    }
+
+    public double getLongitude() {
+	return longitude;
+    }
+
+    public double getDistance(Coordinate other) throws IllegalArgumentException {
+	checkIsNull(other);
+	double lat1Rad = Math.PI * latitude / 180;
+	double lon1Rad = Math.PI * longitude / 180;
+	double lat2Rad = Math.PI * other.getLatitude() / 180;
+	double lon2Rad = Math.PI * other.getLongitude() / 180;
+
+	return Math.acos(Math.sin(lat1Rad) * Math.sin(lat2Rad)
+		+ Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.cos(lon2Rad - lon1Rad)) * 6371000;
+    }
+
+    public double getLatitudinalDistance(Coordinate other) throws IllegalArgumentException {
+	checkIsNull(other);
+	return Math.abs(other.getLatitude() - latitude);
+    }
+
+    public double getLongitudinalDistance(Coordinate other) throws IllegalArgumentException {
+	checkIsNull(other);
+	return Math.abs(other.getLongitude() - longitude);
+    }
+
+    private void checkIsNull(Coordinate other) throws IllegalArgumentException {
+	if (other == null) {
+	    throw new IllegalArgumentException("Other coordinate must not be null");
 	}
-	
-	public double getLatitude()
-	{
-		return latitude;
-	}
-	
-	public double getLongitude()
-	{
-		return longitude;
-	}
-	
-	public double getMeterDistance(Coordinate other) throws IllegalArgumentException
-	{
-		checkIsValid(other);
-		double lat1_rad = Math.PI*latitude/180;
-		double lon1_rad = Math.PI*longitude/180;
-		double lat2_rad = Math.PI*other.getLatitude()/180;
-		double lon2_rad = Math.PI*other.getLongitude()/180;
-		
-		return Math.acos(Math.sin(lat1_rad)*Math.sin(lat2_rad) + Math.cos(lat1_rad)*Math.cos(lat2_rad)*Math.cos(lon2_rad-lon1_rad))*6371000;
-	}
-	
-	public Coordinate getDistance(Coordinate other) throws IllegalArgumentException
-	{
-		checkIsValid(other);
-		double latDiff = getLatitudinalDistance(other);
-		double lonDiff = getLongitudinalDistance(other);
-		
-		return new Coordinate(latDiff, lonDiff, true);
-	}
-	
-	public double getLatitudinalDistance(Coordinate other) throws IllegalArgumentException
-	{
-		checkIsValid(other);
-		return Math.abs(other.getLatitude() - latitude);
-	}
-	
-	public double getLongitudinalDistance(Coordinate other) throws IllegalArgumentException
-	{
-		checkIsValid(other);
-		return Math.abs(other.getLongitude() - longitude);
-	}
-	
-	private void checkIsValid(Coordinate other) throws IllegalArgumentException
-	{
-		if(other == null)
-		{
-			throw new IllegalArgumentException("Other coordinate must not be null");
-		}
-		if(other.isResult)
-		{
-			throw new IllegalArgumentException("Do not reuse coordinates returned from Coordinate::getDistance() they are not coordinates!");
-		}
-	}
+    }
 }
--- src/test/java/org/wahlzeit/model/CoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -2,7 +2,6 @@ package org.wahlzeit.model;
 
 import org.junit.Test;
 
-
 import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -15,101 +14,87 @@ import static org.hamcrest.CoreMatchers.*;
  */
 public class CoordinateTest {
 
-	private Coordinate coordinateCenter;
-	private Coordinate coordinateOne;
-	private Coordinate coordinateTwo;
-	private Coordinate coordinateThree;
+    private Coordinate coordinateCenter;
+    private Coordinate coordinateOne;
+    private Coordinate coordinateTwo;
+    private Coordinate coordinateThree;
 
-	@Before
-	public void initCoordinate() {
-		coordinateCenter = new Coordinate(0,0);
-		coordinateOne = new Coordinate(5.5,10.5);
-		coordinateTwo = new Coordinate(-5.5,-10.5);
-		coordinateThree = new Coordinate(-10.5,10.5);
+    @Before
+    public void initCoordinate() {
+	coordinateCenter = new Coordinate(0, 0);
+	coordinateOne = new Coordinate(5.5, 10.5);
+	coordinateTwo = new Coordinate(-5.5, -10.5);
+	coordinateThree = new Coordinate(-10.5, 10.5);
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testLongitudinalDistance() {
+	assertEquals(coordinateCenter.getLongitudinalDistance(coordinateOne), 10.5, 0);
+	assertEquals(coordinateCenter.getLongitudinalDistance(coordinateTwo), 10.5, 0);
+	assertEquals(coordinateOne.getLongitudinalDistance(coordinateTwo), 21, 0);
+    }
+
+    @Test
+    public void testLatitudinalDistance() {
+	assertEquals(coordinateCenter.getLatitudinalDistance(coordinateOne), 5.5, 0);
+	assertEquals(coordinateCenter.getLatitudinalDistance(coordinateTwo), 5.5, 0);
+	assertEquals(coordinateOne.getLatitudinalDistance(coordinateTwo), 11, 0);
+    }
+
+    @Test
+    public void testDistance() {
+	assertEquals(coordinateTwo.getDistance(coordinateThree), 2377262, 1);
+    }
+
+    @Test
+    public void testEquals() {
+	assertEquals(coordinateCenter, coordinateCenter);
+	assertEquals(coordinateOne, coordinateOne);
+	assertEquals(coordinateCenter, new Coordinate());
+	assertThat(coordinateCenter, not(equalTo(coordinateOne)));
+	assertThat(coordinateOne, not(equalTo(coordinateTwo)));
+	assertThat(coordinateCenter, not(equalTo(null)));
+    }
+
+    @Test
+    public void nullCoordinateShouldThrowException() {
+	try {
+	    coordinateCenter.getLongitudinalDistance(null);
+	    fail("getLongitudinalDistance should throw when passed null");
+	} catch (IllegalArgumentException e) {
 	}
-	
-	/**
-	 *
-	 */
-	@Test
-	public void testLongitudinalDistance() {
-		assertEquals(coordinateCenter.getLongitudinalDistance(coordinateOne),10.5,0);
-		assertEquals(coordinateCenter.getLongitudinalDistance(coordinateTwo),10.5,0);
-		assertEquals(coordinateOne.getLongitudinalDistance(coordinateTwo),21,0);
+	try {
+	    coordinateCenter.getLatitudinalDistance(null);
+	    fail("getLatitudinalDistance should throw when passed null");
+	} catch (IllegalArgumentException e) {
 	}
-	
-	@Test
-	public void testLatitudinalDistance() {
-		assertEquals(coordinateCenter.getLatitudinalDistance(coordinateOne),5.5,0);
-		assertEquals(coordinateCenter.getLatitudinalDistance(coordinateTwo),5.5,0);
-		assertEquals(coordinateOne.getLatitudinalDistance(coordinateTwo),11,0);
+	try {
+	    coordinateCenter.getDistance(null);
+	    fail("getDistance should throw when passed null");
+	} catch (IllegalArgumentException e) {
 	}
-	
-	@Test
-	public void testDistance() {
-		assertEquals(coordinateOne.getDistance(coordinateThree).getLatitude(),16,0);
-		assertEquals(coordinateOne.getDistance(coordinateThree).getLongitude(),0,0);
-		
-		assertEquals(coordinateOne.getDistance(coordinateTwo).getLatitude(),11,0);
-		assertEquals(coordinateOne.getDistance(coordinateTwo).getLongitude(),21,0);
-		
-		assertEquals(coordinateTwo.getMeterDistance(coordinateThree), 2377262,1);
-	}
-	
-	@Test
-	public void testEquals() {
-		assertEquals(coordinateCenter, coordinateCenter);
-		assertEquals(coordinateOne, coordinateOne);
-		assertEquals(coordinateCenter, new Coordinate());
-		assertThat(coordinateCenter, not(equalTo(coordinateOne)));
-		assertThat(coordinateOne, not(equalTo(coordinateTwo)));
-		assertThat(coordinateCenter, not(equalTo(null)));
-	}
-	
-	@Test
-	public void nullCoordinateShouldThrowException() {
-		try
-		{
-			coordinateCenter.getLongitudinalDistance(null);
-			fail("getLongitudinalDistance should throw when passed null");
-		}
-		catch(IllegalArgumentException e){}
-		try
-		{
-			coordinateCenter.getLatitudinalDistance(null);
-			fail("getLatitudinalDistance should throw when passed null");
-		}
-		catch(IllegalArgumentException e){}
-		try
-		{
-			coordinateCenter.getDistance(null);
-			fail("getDistance should throw when passed null");
-		}
-		catch(IllegalArgumentException e){}	
-	}
-	@Test(expected = IllegalArgumentException.class)
-	public void usingResultShouldThrowException() {
-		coordinateThree.getDistance(coordinateOne.getDistance(coordinateTwo));
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void tooLargeLatShouldThrowException() {
-		new Coordinate(120,0);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void tooSmallLatShouldThrowException() {
-		new Coordinate(-120,0);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void tooLargeLonShouldThrowException() {
-		new Coordinate(0,200);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void tooSmallLonShouldThrowException() {
-		new Coordinate(0,-200);
-	}
-	
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void tooLargeLatShouldThrowException() {
+	new Coordinate(120, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void tooSmallLatShouldThrowException() {
+	new Coordinate(-120, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void tooLargeLonShouldThrowException() {
+	new Coordinate(0, 200);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void tooSmallLonShouldThrowException() {
+	new Coordinate(0, -200);
+    }
 }
